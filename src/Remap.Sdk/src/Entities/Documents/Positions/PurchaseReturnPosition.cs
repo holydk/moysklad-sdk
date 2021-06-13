5555e00b250a2f4f37609dcc4fde361f7fc0101c
Dmitry Kiselev <omigoneo@gmail.com>
2021-06-13 17:08:04 +0300

    Add PurchaseReturn API

---
+++ src/Remap.Sdk/src/Entities/Documents/Positions/PurchaseReturnPosition.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Confiti.MoySklad.Remap.Entities
+{
+    /// <summary>
+    /// Represents an purchase return position.
+    /// </summary>
+    public class PurchaseReturnPosition : DocumentPosition
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the discount.
+        /// </summary>
+        /// <value>The discount.</value>
+        [JsonProperty("discount")]
+        public double? Discount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vat.
+        /// </summary>
+        /// <value>The vat.</value>
+        [JsonProperty("vat")]
+        public int? Vat { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serial numbers.
+        /// </summary>
+        /// <value>The serial numbers.</value>
+        [JsonProperty("things")]
+        public string[] Things { get; set; }
+
+        #endregion
+    }
+}
