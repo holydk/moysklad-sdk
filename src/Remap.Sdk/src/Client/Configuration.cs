cfd9401cb06cde6e28ce192fe43d6ae70ed6e639
holydk <omigoneo@gmail.com>
2021-06-17 14:59:31 +0300

    Pin version to 0.7.0

--- src/Remap.Sdk/src/Client/Configuration.cs
+++ src/Remap.Sdk/src/Client/Configuration.cs
@@ -84,7 +84,7 @@ namespace Confiti.MoySklad.Remap.Client
         /// <summary>
         /// The version of the package.
         /// </summary>
-        public const string Version = "0.6.0";
+        public const string Version = "0.7.0";
             
         #endregion
 
