f38d5b2bc93748fb9d6776b10bbffc6bce988bb4
Dmitry Kiselev <omigoneo@gmail.com>
2022-03-15 18:39:30 +0300

    Pin version to 0.10.0

--- src/Remap.Sdk/src/Client/ApiDefaults.cs
+++ src/Remap.Sdk/src/Client/ApiDefaults.cs
@@ -7,6 +7,6 @@ namespace Confiti.MoySklad.Remap.Client
     {
         internal const string DEFAULT_BASE_PATH = "https://online.moysklad.ru";
         internal const string DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-        internal const string DEFAULT_USER_AGENT = "Confiti-Remap-Sdk/0.9.1";
+        internal const string DEFAULT_USER_AGENT = "Confiti-Remap-Sdk/0.10.0";
     }
 }
